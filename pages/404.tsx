3bde69049daf3d558c9ef2affe51d8c342d4a289
Mine77 <haichaozhu77@gmail.com>
2022-01-10 15:23:20 +0800

    add 404

---
+++ pages/404.tsx
@@ -0,0 +1,20 @@
+const custom404 = () => {
+  return (
+    <div className="relative overflow-hidden h-screen flex justify-center items-center">
+      <img src="/404-bg.svg" className="absolute h-full w-full object-cover" />
+      <div className="inset-0 bg-black opacity-25 absolute"></div>
+      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div className="w-full font-mono flex flex-col items-center relative z-10">
+          <h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+            You&#x27;re alone here
+          </h1>
+          <p className="font-extrabold text-8xl my-44 text-white animate-bounce">
+            404
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default custom404;
