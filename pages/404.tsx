e49202c31094890ad64bffa8b19908cf9197971f
Mine77 <haichaozhu77@gmail.com>
2022-01-12 00:14:49 +0800

    change 404 img to Image

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image"
 const custom404 = () => {
   return (
     <div className="relative overflow-hidden h-screen flex justify-center items-center">
-      <img src="/404-bg.svg" className="absolute h-full w-full object-cover" />
+      <Image layout="fill" alt="404image"  src="/404-bg.svg" className="absolute h-full w-full object-cover" />
       <div className="inset-0 bg-black opacity-25 absolute"></div>
       <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
         <div className="w-full font-mono flex flex-col items-center relative z-10">
