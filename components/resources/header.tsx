5779cbacdb6cc0ee2b7248fb200a23a62bd42f11
Mine77 <haichaozhu77@gmail.com>
2022-01-14 21:33:21 +0800

    update header style

--- components/resources/header.tsx
+++ components/resources/header.tsx
@@ -1,14 +1,11 @@
-const ResHeader = (props:{title:string,description:string}) => {
-    return (
-        <div className="pt-6 pb-4">
-        <h1 className="text-6xl font-extrabold pt-3">
-          {props.title}
-        </h1>
-        <p className="text-xl text-gray-500 pt-3">
-          {props.description}
-        </p>
+const ResHeader = (props: { title: string; description: string }) => {
+  return (
+    <div className="pt-6 pb-4 flex flex-row">
+      <div className="flex flex-col">
+        <h1 className="text-6xl font-extrabold pt-3">{props.title}</h1>
+        <p className="text-xl text-gray-500 pt-3">{props.description}</p>
       </div>
-    );
-  };
-  export default ResHeader;
-  
+    </div>
+  );
+};
+export default ResHeader;
