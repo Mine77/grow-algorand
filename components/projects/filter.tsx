17534d760669186b54db2a04e285dad4c3d70346
Mine77 <haichaozhu77@gmail.com>
2022-01-15 23:15:01 +0800

    update

--- components/projects/filter.tsx
+++ components/projects/filter.tsx
@@ -1,25 +1,5 @@
 import Link from "next/link";
-import {
-  AnchorHTMLAttributes,
-  createRef,
-  DetailedHTMLProps,
-  MouseEventHandler,
-  useRef,
-  useState,
-} from "react";
-
-interface FilterState {
-  category: {
-    all: boolean;
-    application: boolean;
-    devTools: boolean;
-  };
-  Tags: {
-    all: boolean;
-    application: boolean;
-    devTools: boolean;
-  };
-}
+import { MouseEventHandler } from "react";
 
 const unSelectedStyle =
   "hover:text-gray-800 hover:bg-blue-100  p-2 mx-1 my-2 transition-colors duration-200 flex flex-row items-center text-gray-500 ml-2 text-md font-normal rounded-xl w-full";
@@ -34,23 +14,11 @@ interface Props {
 }
 
 const Filter = (props: Props) => {
-  const ref = useRef(null);
-  const sideBarList = [
-    {
-      sectionTitle: "Category",
-      list: props.filterItem.category,
-    },
-    {
-      sectionTitle: "Tags",
-      list: props.filterItem.tags,
-    },
-  ];
   const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
     const target = event.target as Element;
     target.className =
       target.className === seletedStyle ? unSelectedStyle : seletedStyle;
     if (target.id === "Category.All") {
-      (ref.current as unknown as Element).className = unSelectedStyle;
       console.log(target.id);
     }
   };
@@ -59,31 +27,50 @@ const Filter = (props: Props) => {
       <div className="flex flex-col">
         <div className="w-56 h-full">
           <nav className="px-6">
-            {sideBarList.map((section, i) => (
-              <div key={i}>
-                <p className="text-gray-800 ml-2 w-full border-b-2 pb-2 border-gray-100 mb-4 text-lg font-bold">
-                  {section.sectionTitle}
-                </p>
+            <div>
+              <p className="text-gray-800 ml-2 w-full border-b-2 pb-2 border-gray-100 mb-4 text-lg font-bold">
+                Category
+              </p>
+              <button
+                id={"Category.All"}
+                className={seletedStyle}
+                onClick={handleClick}
+              >
+                All
+              </button>
+              {props.filterItem.category.map((item, j) => (
                 <button
-                  id={`${section.sectionTitle}.All`}
-                  className={seletedStyle}
+                  key={j}
+                  id={`Category.${item}`}
+                  className={unSelectedStyle}
                   onClick={handleClick}
                 >
-                  All
+                  {item}
                 </button>
-                {section.list.map((item, j) => (
-                  <button
-                    key={j}
-                    ref={ref}
-                    id={`${section.sectionTitle}.${item}`}
-                    className={unSelectedStyle}
-                    onClick={handleClick}
-                  >
-                    {item}
-                  </button>
-                ))}
-              </div>
-            ))}
+              ))}
+            </div>
+            <div>
+              <p className="text-gray-800 ml-2 w-full border-b-2 pb-2 border-gray-100 mb-4 text-lg font-bold">
+                Tags
+              </p>
+              <button
+                id={"Tags.All"}
+                className={seletedStyle}
+                onClick={handleClick}
+              >
+                All
+              </button>
+              {props.filterItem.tags.map((item, j) => (
+                <button
+                  key={j}
+                  id={`Tags.${item}`}
+                  className={unSelectedStyle}
+                  onClick={handleClick}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
           </nav>
         </div>
       </div>
